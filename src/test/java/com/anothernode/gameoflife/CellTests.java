b089cf1d8c8e89b97c4ff8e8ff81a2c1a332a052
Moritz Reiter <moritz@anothernode.com>
2019-11-24 16:19:53 +0100

    Initialize

---
+++ src/test/java/com/anothernode/gameoflife/CellTests.java
@@ -0,0 +1,12 @@
+package com.anothernode.gameoflife;
+
+import org.junit.jupiter.api.Test;
+
+public class CellTests {
+
+    @Test
+    public void cellExists() {
+
+        Cell cell = new Cell();
+    }
+}
