89d6abe3da9c847daa7d1fc40eb1815a471b9f90
Moritz Reiter <moritz@anothernode.com>
2020-09-24 12:02:48 +0200

    Implement test for game creation

--- src/main/java/com/anothernode/gameoflife/WebController.java
+++ src/main/java/com/anothernode/gameoflife/WebController.java
@@ -2,6 +2,7 @@ package com.anothernode.gameoflife;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -14,6 +15,11 @@ public class WebController {
 
   @GetMapping("/")
   public String index() {
+    return "index";
+  }
+
+  @PostMapping("/games")
+  public String createGame() {
     return "index";
   }
 }
