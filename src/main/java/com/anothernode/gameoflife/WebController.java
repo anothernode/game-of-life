9d1b1bc44f93f9239bb76cfb31d986232e113cbc
Moritz Reiter <moritz@anothernode.com>
2020-10-15 09:24:11 +0200

    Game creation via POST (#2)
    * Add constructor to create Game with start conf
    
    * Add example JSON Game representation
    
    * Add mapping to create a game via POST

--- src/main/java/com/anothernode/gameoflife/WebController.java
+++ src/main/java/com/anothernode/gameoflife/WebController.java
@@ -1,8 +1,11 @@
 package com.anothernode.gameoflife;
 
+import java.util.Set;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -21,5 +24,10 @@ public class WebController {
   @PostMapping("/games")
   public String createGame() {
     return "index";
+  }
+
+  @PostMapping(value = "/games", produces = MediaType.APPLICATION_JSON_VALUE)
+  public @ResponseBody Game createGameJson(@RequestBody Game game) {
+    return new Game(game.cells());
   }
 }
