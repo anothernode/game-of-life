30846e8d9cbab0769486e05c9a466c5f1829ef96
Moritz Reiter <moritz@anothernode.com>
2020-10-16 19:37:41 +0200

    Fix indentation

--- src/main/java/com/anothernode/gameoflife/Game.java
+++ src/main/java/com/anothernode/gameoflife/Game.java
@@ -4,37 +4,38 @@ import java.util.Set;
 
 public class Game {
 
-    private Board board;
+  private Board board;
 
-    public Game() {}
+  public Game() {
+  }
 
-    public Game(Board board) {
-        this.board = board;
-    }
+  public Game(Board board) {
+    this.board = board;
+  }
 
-    /**
-     * Creates a <i>Game</i> with a given start configuration expressed as as set of the
-     * {@link Cell}s on the {@link Board} in {@link Round} zero (before the first iteration).
-     *
-     * @param cells the living {@link Cell}s before the first iteration
-     */
-    public Game(Set<Cell> cells) {
-        this.board = new Board();
-        cells.forEach((cell -> board.add(cell)));
-    }
+  /**
+   * Creates a <i>Game</i> with a given start configuration expressed as as set of the {@link Cell}s
+   * on the {@link Board} in {@link Round} zero (before the first iteration).
+   *
+   * @param cells the living {@link Cell}s before the first iteration
+   */
+  public Game(Set<Cell> cells) {
+    this.board = new Board();
+    cells.forEach((cell -> board.add(cell)));
+  }
 
-    public Board getBoard() {
-        return board;
-    }
+  public Board getBoard() {
+    return board;
+  }
 
-    public Set<Cell> cells() {
-        return board.getCells();
-    }
+  public Set<Cell> cells() {
+    return board.getCells();
+  }
 
-    /**
-     * Just a stub
-     */
-    public void iterate() {
-        board.getCells().clear();
-    }
+  /**
+   * Just a stub
+   */
+  public void iterate() {
+    board.getCells().clear();
+  }
 }
