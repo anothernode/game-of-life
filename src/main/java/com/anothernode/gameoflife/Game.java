3fce7085f6549090c3615609e87ee6ac39297cc0
Moritz Reiter <moritz@anothernode.com>
2020-10-17 20:05:33 +0200

    Game creation via POST continued (#3)
    * Fix indentation
    
    * Add Apache Commons Lang library
    
    * Add game repository
    
    * Actually set id for Game

--- src/main/java/com/anothernode/gameoflife/Game.java
+++ src/main/java/com/anothernode/gameoflife/Game.java
@@ -1,40 +1,47 @@
 package com.anothernode.gameoflife;
 
 import java.util.Set;
+import org.apache.commons.lang3.RandomStringUtils;
 
 public class Game {
 
-    private Board board;
+  private String id = RandomStringUtils.random(10);
 
-    public Game() {}
+  private Board board = new Board();
 
-    public Game(Board board) {
-        this.board = board;
-    }
+  public Game() {
+  }
 
-    /**
-     * Creates a <i>Game</i> with a given start configuration expressed as as set of the
-     * {@link Cell}s on the {@link Board} in {@link Round} zero (before the first iteration).
-     *
-     * @param cells the living {@link Cell}s before the first iteration
-     */
-    public Game(Set<Cell> cells) {
-        this.board = new Board();
-        cells.forEach((cell -> board.add(cell)));
-    }
+  public Game(Board board) {
+    this.board = board;
+  }
 
-    public Board getBoard() {
-        return board;
-    }
+  /**
+   * Creates a <i>Game</i> with a given start configuration expressed as as set of the {@link Cell}s
+   * on the {@link Board} in {@link Round} zero (before the first iteration).
+   *
+   * @param cells the living {@link Cell}s before the first iteration
+   */
+  public Game(Set<Cell> cells) {
+    cells.forEach((cell -> board.add(cell)));
+  }
 
-    public Set<Cell> cells() {
-        return board.getCells();
-    }
+  public Board getBoard() {
+    return board;
+  }
 
-    /**
-     * Just a stub
-     */
-    public void iterate() {
-        board.getCells().clear();
-    }
+  public String getId() {
+    return id;
+  }
+
+  public Set<Cell> cells() {
+    return board.getCells();
+  }
+
+  /**
+   * TODO
+   */
+  public void iterate() {
+    board.getCells().clear();
+  }
 }
