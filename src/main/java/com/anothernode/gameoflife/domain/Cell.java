efeb531a94bdd86e68dece3612de155d667a4400
Moritz Reiter <moritz@anothernode.com>
2021-02-22 10:02:51 +0100

    Fix indentation

--- src/main/java/com/anothernode/gameoflife/domain/Cell.java
+++ src/main/java/com/anothernode/gameoflife/domain/Cell.java
@@ -4,50 +4,57 @@ import static java.text.MessageFormat.format;
 
 public class Cell implements Comparable<Cell> {
 
-    private Square square;
+  private Square square;
 
-    public Cell() {}
+  public Cell() {
+  }
 
-    public Cell(Square square) {
-        this.square = square;
+  public Cell(Square square) {
+    this.square = square;
+  }
+
+  public Cell(int x, int y) {
+    this.square = Square.create(x, y);
+  }
+
+  public Square getSquare() {
+    return square;
+  }
+
+  @Override
+  public boolean equals(Object that) {
+    if (!(that instanceof Cell)) {
+      return false;
+    } else {
+      return this.square.equals(((Cell) that).getSquare());
     }
+  }
 
-    public Cell(int x, int y) {
-        this.square = Square.create(x, y);
-    }
+  @Override
+  public int hashCode() {
+    return this.getSquare().hashCode();
+  }
 
-    public Square getSquare() {
-        return square;
-    }
+  @Override
+  public String toString() {
+    return format("Cell[x = {0}, y = {1}]",
+        getSquare().getX(),
+        getSquare().getY());
+  }
 
-    @Override
-    public boolean equals(Object that) {
-        if (!(that instanceof Cell)) {
-            return false;
-        } else {
-            return this.square.equals(((Cell) that).getSquare());
-        }
-    }
-
-    @Override
-    public int hashCode() {
-        return this.getSquare().hashCode();
-    }
-
-    @Override
-    public String toString() {
-        return format("Cell[x = {0}, y = {1}]",
-                getSquare().getX(),
-                getSquare().getY());
-    }
-
-    @Override
-    public int compareTo(Cell that) {
-        if (this.equals(that)) return 0;
-        if (this.square.getY() < that.square.getY()) return -1;
-        if (this.square.getY() > that.square.getY()) return 1;
-        if (this.square.getX() < that.square.getX()) return -1;
-        if (this.square.getX() > that.square.getX()) return 1;
-        return 0;
-    }
+  @Override
+  public int compareTo(Cell that) {
+    if (this.equals(that))
+      return 0;
+    // I guess this belongs to the Square (Tell, don't ask)
+    if (this.square.getY() < that.square.getY())
+      return -1;
+    if (this.square.getY() > that.square.getY())
+      return 1;
+    if (this.square.getX() < that.square.getX())
+      return -1;
+    if (this.square.getX() > that.square.getX())
+      return 1;
+    return 0;
+  }
 }
